dbe20a6cb0d9aaf3ae3fd122d5089e467c840499
Marius <119943053+marius-maur@users.noreply.github.com>
2023-01-10 13:46:23 +0100

    Update sieb.c

--- sieb.c
+++ sieb.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+  void befuelle_array(int array[]){
+    int size = sizeof(array[])/sizeof(array[0]);
+    for(int i; i <= s; i++){
+    array[i] = 1+i;
+    }
+    
+  void gib_primzahlen(int array[]){
+    int size = sizeof(array[])/sizeof(array[0]);
+    
+    for(int i; i<= s/2; i++){
+      if(%array[i]/i+1 != 0){
+      printf(" %i ", array[i]);
+      }
+    }
+
+  int main(){
+    int zahlen[300];
+    befuelle_array(zahlen[]);
+    gib_primzahlen(zahlen[]);
+  }
